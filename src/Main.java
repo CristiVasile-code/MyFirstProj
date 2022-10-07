4a803c7e1c27db06ca1821a0048dc92a53084c45
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-10-07 16:40:24 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         System.out.println(myByteVariable);
 
-        myByteVariable += 2; // myByteVariable = myByteVariable + 2;
+        //myByteVariable += 2; // myByteVariable = myByteVariable + 2;
 
         System.out.println(myByteVariable);
 
