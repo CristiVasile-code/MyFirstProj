e685d6c142c7b2a7c1e6166cad4f38742c055f75
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-10-10 21:23:28 +0300

    metode calculator in clasa Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    public static int sum(int first, int second) {
+        int result = first + second;
+        return result;
+    }
+
+    public static float substract(float first, float second){
+        float result = first - second;
+        return result;
+    }
+
+    public static float divide(float first,float second){
+        float result = first / second;
+        return result;
+    }
+
+    public static float multi(float first, float second){
+        float result = first * second;
+        return result;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,22 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
+        Calculator calc = new Calculator();
         int x = 2;
         int y = 5;
 
-        int z = sum(x, y);
-        System.out.println(z);
-
-        float a = substract(x, y);
+        float a = calc.sum(x, y);
         System.out.println(a);
-        a = divide(x,y);
+        a = calc.substract(x, y);
         System.out.println(a);
-        a = multi(x,y);
+        a = calc.divide(x, y);
+        System.out.println(a);
+        a = calc.multi(x, y);
         System.out.println(a);
 
-    }
-
-    public static int sum(int first, int second) {
-        int result = first + second;
-        return result;
-    }
-
-    public static float substract(float first, float second){
-        float result = first - second;
-        return result;
-    }
-
-    public static float divide(float first,float second){
-        float result = first / second;
-        return result;
-    }
-
-    public static float multi(float first, float second){
-        float result = first * second;
-        return result;
     }
 }
+
 
 
 
