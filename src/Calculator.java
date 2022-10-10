cec68115e6d2ceef5bfbe4cae512596468164449
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-10-10 21:25:59 +0300

    metode calculator in clasa Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,20 +1,20 @@
 public class Calculator {
-    public static int sum(int first, int second) {
-        int result = first + second;
+    public float sum(float first, float second) {
+        float result = first + second;
         return result;
     }
 
-    public static float substract(float first, float second){
+    public float substract(float first, float second){
         float result = first - second;
         return result;
     }
 
-    public static float divide(float first,float second){
+    public float divide(float first,float second){
         float result = first / second;
         return result;
     }
 
-    public static float multi(float first, float second){
+    public float multi(float first, float second){
         float result = first * second;
         return result;
     }
