2bc7a701af37982314ab1613457e3a6954254d43
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-10-10 20:00:52 +0300

    metode calculator

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+     /*   System.out.println("Hello world!");
 
         byte myByteVariable = 100;
         String myString = " ceva ";
 
         System.out.println(myByteVariable);
 
-        //myByteVariable += 2; // myByteVariable = myByteVariable + 2;
+        myByteVariable += 21; // myByteVariable = myByteVariable + 2;
 
         System.out.println(myByteVariable);
 
@@ -25,10 +25,7 @@ public class Main {
 
         System.out.println("rezultatul este: " + (x + 100 + 200));
 
-        double a = 123123.123;
-        float q = 123.123F;
-
-        long o = 12311231231231L;
+       
 
         String author = "Cosmin" + "" + " " + "";
         int duration = 20;
@@ -46,12 +43,44 @@ public class Main {
         int i = 2;
         i += i += i++ + ++i;
         System.out.println(i++);
-        System.out.println(i);
+        System.out.println(i); */
 
+        int x = 2;
+        int y = 5;
 
+        int z = sum(x, y);
+        System.out.println(z);
+
+        float a = substract(x, y);
+        System.out.println(a);
+        a = divide(x,y);
+        System.out.println(a);
+        a = multi(x,y);
+        System.out.println(a);
 
     }
 
+    public static int sum(int first, int second) {
+        int result = first + second;
+        return result;
+    }
+
+    public static float substract(float first, float second){
+        float result = first - second;
+        return result;
+    }
+
+    public static float divide(float first,float second){
+        float result = first / second;
+        return result;
+    }
+
+    public static float multi(float first, float second){
+        float result = first * second;
+        return result;
+    }
 }
+
+
 
 
